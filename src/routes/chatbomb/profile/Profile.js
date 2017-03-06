1e7c22934f8d8e9c4bf87090c458f15fa6b7bbd9
Jon Savage Kathe <jonsavagek36@gmail.com>
2017-03-06 07:37:04 -0500

    Update Profile.js

--- src/routes/chatbomb/profile/Profile.js
+++ src/routes/chatbomb/profile/Profile.js
@@ -1,34 +1,22 @@
 import React, { Component } from 'react';
 
-import ViewProfile from './ViewProfile';
-import EditProfile from './EditProfile';
-
-class Profile extends Component {
+export default class Profile extends Component {
   constructor(props) {
     super(props);
     this.state = {
       view: 'profile'
-    };
+    }
   }
 
   render() {
-    let view;
-    if (this.state.view == 'profile') {
-      view = <ViewProfile profile={this.props.profile} />;
-    } else if (this.state.view == 'edit') {
-      view = <EditProfile />;
-    }
     return (
       <div className='main-body'>
         <div className='page-title'>
           <div className='banner'>Profile</div>
         </div>
         <div className='profile-body'>
-          {view}
         </div>
       </div>
     );
   }
 }
-
-export default Profile;
