ea2819fe8b9d17cf3ad2434d391ddf093d6dfe40
Jon Savage Kathe <jonsavagek36@gmail.com>
2017-03-06 07:36:30 -0500

    Update ViewRequests.js

--- src/routes/chatbomb/requests/ViewRequests.js
+++ src/routes/chatbomb/requests/ViewRequests.js
@@ -2,18 +2,16 @@ import React, { Component } from 'react';
 
 import Requestplate from './Requestplate';
 
-class ViewRequests extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
+export default class ViewRequests extends Component {
   render() {
     let requests = null;
     if (this.props.requests !== undefined) {
       requests = this.props.requests.map((request, idx) => {
         let acceptRequest = () => {
-          this.props.acceptRequest(request);
+          let new_friends = this.props.acceptRequest(request);
+          if (new_friends !== undefined) {
+            this.props.updateFriends(new_friends);
+          }
         }
         return <Requestplate request={request} acceptRequest={acceptRequest} key={idx} />;
       });
@@ -25,5 +23,3 @@ class ViewRequests extends Component {
     );
   }
 }
-
-export default ViewRequests;
