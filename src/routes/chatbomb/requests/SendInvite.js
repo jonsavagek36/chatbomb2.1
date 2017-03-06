5cbd75f04ce593360bbc9ef058abc4a5a0bf1896
Jon Savage Kathe <jonsavagek36@gmail.com>
2017-03-06 07:35:57 -0500

    Update SendInvite.js

--- src/routes/chatbomb/requests/SendInvite.js
+++ src/routes/chatbomb/requests/SendInvite.js
@@ -1,28 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class SendInvite extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className='send-invite'>
-        <div className='form-title'>Invite a friend!</div>
-        <div className='form-box'>
-          <div className='form-row'>
-            <div className='form-label'>Friend E-Mail: </div>
-            <div className='form-input'><input type='text' id='invite-email' /></div>
-          </div>
-          <div className='form-buttons'>
-            <div className='signup-btn'><button onClick={this.props.sendInvite}>Send Invite</button></div>
-          </div>
-          <div className='form-message' id='invite-message'></div>
-        </div>
+const SendInvite = (props) => {
+  return (
+    <div className='send-invite'>
+      <div className='form-title'>Invite a friend!</div>
+      <div className='form-box'>
+        <div className='form-label'>Friend E-Mail: </div>
+        <div className='form-input'><input type='text' id='invite-email' /></div>
       </div>
-    );
-  }
+      <div className='form-buttons'>
+        <div className='signup-btn'><button onClick={props.sendInvite}>Send Invite</button></div>
+      </div>
+      <div className='form-message' id='invite-message'></div>
+    </div>
+  );
 }
 
 export default SendInvite;
