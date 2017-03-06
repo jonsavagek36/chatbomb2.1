b27a57aade4eb6c3e99eb7095db21e89ada3fd2e
Jon Savage Kathe <jonsavagek36@gmail.com>
2017-03-06 07:27:26 -0500

    Update mailer.js

--- api/v1/mailer.js
+++ api/v1/mailer.js
@@ -4,13 +4,25 @@ let smtpTransport = nodemailer.createTransport({
   service: 'gmail',
   host: 'smtp.gmail.com',
   auth: {
-    user: '',
-    pass: ''
+    user: 'osmochat',
+    pass: process.env.MAIL_PWD
   }
 });
 
+exports.send_confirm = function(user) {
+  let message = `Please <a href='https://chat-bomb.herokuapp.com/confirm?id=${user.id}'>CLICK HERE</a> to confirm your Chatbomb account!`;
+  let mailOptions = {
+    to: user.email,
+    subject: `CHATBOMB Confirmation`,
+    html: message
+  };
+  smtpTransport.sendMail(mailOptions, function(err, res) {
+    if (err) throw err;
+  });
+}
+
 exports.send_invite = function(req, res) {
-  let message = `${req.user.screen_name} has invited you to join Chatbomb.  <a href='http://localhost:5000/join?code=${req.user.id}'>Click Here</a>`;
+  let message = `${req.user.screen_name} has invited you to join Chatbomb.  <a href='https://chat-bomb.herokuapp.com/invite_join?code=req.user.id'>CLICK HERE</a> to join!`;
   let mailOptions = {
     to: req.body.invite_email,
     subject: `You've been CHATBOMBED`,
@@ -18,9 +30,10 @@ exports.send_invite = function(req, res) {
   };
   smtpTransport.sendMail(mailOptions, function(err, res) {
     if (err) {
-      console.log(err);
+      res.json({ message: 'Invite failure' });
+      throw err;
     } else {
-      res.json({ message: 'Invite sent!' });
+      res.json({ message: 'Invite sent' });
     }
   });
 }
