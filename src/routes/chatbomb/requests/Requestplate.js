b2e0a7d3940f3d5c04a970113e443200a2fc9b92
Jon Savage Kathe <jonsavagek36@gmail.com>
2017-03-06 07:35:38 -0500

    Update Requestplate.js

--- src/routes/chatbomb/requests/Requestplate.js
+++ src/routes/chatbomb/requests/Requestplate.js
@@ -1,25 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Requestplate extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className='request-plate'>
-        <div className='request-avatar'></div>
-        <div className='request-info'>
-          <div className='request-name'>{this.props.request.request_name}</div>
-          <div className='request-email'>{this.props.request.request_email}</div>
-        </div>
-        <div className='accept-request'>
-          <div className='accept-button'><button onClick={this.props.acceptRequest}>Accept Request</button></div>
-        </div>
+const Requestplate = (props) => {
+  return (
+    <div className='request-plate'>
+      <div className='request-avatar'></div>
+      <div className='request-info'>
+        <div className='request-name'>{props.request.request_name}</div>
+        <div className='request-email'>{props.request.request_email}</div>
       </div>
-    );
-  }
+      <div className='accept-request'>
+        <div className='accept-button'><button onClick={props.acceptRequest}>Accept Request</button></div>
+      </div>
+    </div>
+  );
 }
 
 export default Requestplate;
