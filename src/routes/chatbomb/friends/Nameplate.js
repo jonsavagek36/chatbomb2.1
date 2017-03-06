5beaa5151629c27bc5dfd339641101286bd14557
Jon Savage Kathe <jonsavagek36@gmail.com>
2017-03-06 07:37:37 -0500

    Update Nameplate.js

--- src/routes/chatbomb/friends/Nameplate.js
+++ src/routes/chatbomb/friends/Nameplate.js
@@ -1,27 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Nameplate extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className='name-plate' onClick={this.props.selectFriend}>
-        <div className='friend-avatar'>
-        </div>
-        <div className='friend-email'>
-          {this.props.friend.email}
-        </div>
-        <div className='friend-name'>
-          {this.props.friend.screen_name}
-        </div>
-        <div className='new-message'>
-        </div>
+const Nameplate = (props) => {
+  return (
+    <div className='name-plate' onClick={props.selectFriend}>
+      <div className='friend-avatar'>
       </div>
-    );
-  }
+      <div className='friend-email'>{props.friend.email}</div>
+      <div className='friend-name'>{props.friend.name}</div>
+      <div className='new-message'></div>
+    </div>
+  );
 }
 
 export default Nameplate;
