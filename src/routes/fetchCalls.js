93b26089f248538a7840540118da919615dfb796
Jon Savage Kathe <jonsavagek36@gmail.com>
2017-03-06 07:33:37 -0500

    Update fetchCalls.js

--- src/routes/fetchCalls.js
+++ src/routes/fetchCalls.js
@@ -1,12 +1,57 @@
-// Main App fetch calls
 import { browserHistory } from 'react-router';
+
+exports.loginUser = function() {
+  let emailNode = document.getElementById('login-email');
+  let passNode = document.getElementById('login-password');
+  let messageNode = document.getElementById('login-message');
+  messageNode.textContent = '';
+  let fetchBody = {
+    email: emailNode.value,
+    password: passNode.value
+  };
+  let myHeaders = newHeaders();
+  myHeaders.append('Content-Type', 'application/json');
+  fetch('/api/v1/users/log_in', {
+    method: 'POST',
+    body: JSON.stringify(fetchBody),
+    headers: myHeaders
+  })
+    .then(response => {
+      if (response.ok) {
+        return response;
+      } else {
+        messageNode.textContent = 'Failed to log in';
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.message) messageNode.textContent = data.message;
+      if (data.token) sessionStorage.setItem('token', data.token);
+      if (data.message && data.token) browserHistory.push('/app/chatbomb');
+    })
+  emailNode.value = '';
+  passNode.value = '';
+}
 
 exports.signUp = function() {
   let emailNode = document.getElementById('signup-email');
   let nameNode = document.getElementById('signup-screenname');
   let passNode = document.getElementById('signup-password');
   let confirmNode = document.getElementById('confirm-password');
-  if (passNode.value == confirmNode.value) {
+  let emailMsg = document.getElementById('email-message');
+  let nameMsg = document.getElementById('screenname-message');
+  let passwordMsg = document.getElementById('password-message');
+  let messageNode = document.getElementById('signup-message');
+  emailMsg.textContent = '';
+  nameMsg.textContent = '';
+  passwordMsg.textContent = '';
+  messageNode.textContent = '';
+  if (emailNode.value == null || emailNode.value == '') emailMsg.textContent = 'Must enter e-mail';
+  if (nameNode.value == null || nameNode.value == '') nameMsg.textContent = 'Must enter screen name';
+  if (passNode.value == null || passNode.value == '') passwordMsg.textContent = 'Must enter password';
+  if (confirmNode.value == null || confirmNode.value == '') passwordMsg.textContent = 'Must confirm password';
+  if (passNode.value != confirmNode.value) passwordMsg.textContent = 'Passwords must match';
+  if (emailNode.value && nameNode.value && passNode.value && confirmNode.value && passNode.value == confirmNode.value) {
     let fetchBody = {
       email: emailNode.value,
       screen_name: nameNode.value,
@@ -23,102 +68,102 @@ exports.signUp = function() {
         if (response.ok) {
           return response;
         } else {
-          console.log('Failed to register.');
+          messageNode.textContent = 'Sign up failure';
         }
       })
       .then(response => response.json())
       .then(data => {
-        console.log(data);
+        browserHistory.push('/app');
+        emailNode.value = '';
+        nameNode.value = '';
+        passNode.value = '';
+        confirmNode.value = '';
       })
-      emailNode.value = '';
-      nameNode.value = '';
-      passNode.value = '';
-      confirmNode.value = '';
-  } else {
-    document.getElementById('signup-message').textContent = 'Passwords must match.';
   }
 }
 
-exports.loginUser = function() {
-  let emailNode = document.getElementById('login-email');
-  let passNode = document.getElementById('login-password');
+exports.sendInvite = function() {
+  let emailNode = document.getElementById('invite-email');
+  let messageNode = document.getElementById('invite-message');
+  messageNode.textContent = '';
+  if (emailNode.value) {
+    let fetchBody = {
+      invite_email: emailNode.value
+    };
+    let token = sessionStorage.getItem('token');
+    let myHeaders = new Headers();
+    myHeaders.append('x-access-token', token);
+    fetch('/api/v1/invite/send', {
+      method: 'POST',
+      body: JSON.stringify(fetchBody),
+      headers: myHeaders
+    })
+      .then(response => {
+        if (response.ok) return response;
+      })
+      .then(response => response.json())
+      .then(data => {
+        if (data.message) messageNode.textContent = data.message;
+        emailNode.value = '';
+      })
+  } else {
+    messageNode.textContent = 'You must input an e-mail address';
+  }
+}
+
+exports.sendRequest = function() {
+  let emailNode = document.getElementById('request-email');
+  let nameNode = document.getElementById('request-name');
+  let msgNode = document.getElementById('req-message');
+  msgNode.textContent = '';
+  if (emailNode.value || nameNode.value) {
+    let fetchBody = {};
+    if (emailNode.value) fetchBody.req_email = emailNode.value;
+    if (nameNode.value) fetchBody.req_name = nameNode.value;
+    let token = sessionStorage.getItem('token');
+    let myHeaders = new Headers();
+    myHeaders.append('x-access-token', token);
+    fetch('/api/v1/requests/send', {
+      method: 'POST',
+      body: JSON.stringify(fetchBody),
+      headers: myHeaders
+    })
+      .then(response => {
+        if (response.ok) {
+          emailNode.value = '';
+          nameNode.value = '';
+        }
+      })
+  } else {
+    msgNode.textContent = 'You must request by e-mail or screen name';
+  }
+}
+
+exports.acceptRequest = function(request_info) {
+  let new_friends = [];
   let fetchBody = {
-    email: emailNode.value,
-    password: passNode.value
+    request: request_info
   };
+  let token = sessionStorage.getItem('token');
   let myHeaders = new Headers();
-  myHeaders.append('Content-Type','application/json');
-  fetch('/api/v1/users/log_in', {
+  myHeaders.append('x-access-token', token);
+  fetch('/api/v1/requests/accept', {
     method: 'POST',
     body: JSON.stringify(fetchBody),
     headers: myHeaders
   })
     .then(response => {
-      if (response.ok) {
-        return response;
-      } else {
-        console.log('Failed to login');
-      }
+      if (response.ok) return response;
     })
     .then(response => response.json())
     .then(data => {
-      sessionStorage.setItem('token', data.token);
-      browserHistory.push('/app/chatbomb');
-    })
-  emailNode.value = '';
-  passNode.value = '';
-}
-
-exports.testAuth = function() {
-  let token = sessionStorage.getItem('token');
-  let myHeaders = new Headers();
-  myHeaders.append('x-access-token', token);
-  fetch('/api/v1/users/testAuth', {
-    method: 'GET',
-    headers: myHeaders
-  })
-    .then(response => {
-      if (response.ok) {
-        return response;
-      } else {
-        console.log('Abject Failure');
+      if (data.friends) {
+        data.friends.forEach(friend => {
+          new_friends.push(friend);
+        });
       }
     })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-    })
-}
-
-exports.getProfile = function() {
-  let profile = {};
-  let token = sessionStorage.getItem('token');
-  let myHeaders = new Headers();
-  myHeaders.append('x-access-token', token);
-  fetch('/api/v1/users/profile', {
-    method: 'GET',
-    headers: myHeaders
-  })
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        console.log('Failed to get profile');
-      }
-    })
-    .then(data => {
-      let id = data.profile.id;
-      let email = data.profile.email;
-      let screen_name = data.profile.screen_name;
-      let friends = data.profile.friends;
-      let requests = data.profile.requests;
-      profile.id = id;
-      profile.email = email;
-      profile.screen_name = screen_name;
-      profile.friends = friends;
-      profile.requests = requests;
-    })
-  return profile;
+  return new_friends;
 }
 
 exports.getFriends = function() {
@@ -131,11 +176,7 @@ exports.getFriends = function() {
     headers: myHeaders
   })
     .then(response => {
-      if (response.ok) {
-        return response;
-      } else {
-        console.log('Failed to get friends');
-      }
+      if (response.ok) return response;
     })
     .then(response => response.json())
     .then(data => {
@@ -148,114 +189,3 @@ exports.getFriends = function() {
   return friends;
 }
 
-exports.getRequests = function() {
-  let requests = [];
-  let token = sessionStorage.getItem('token');
-  let myHeaders = new Headers();
-  myHeaders.append('x-access-token', token);
-  fetch('/api/v1/users/requests', {
-    method: 'GET',
-    headers: myHeaders
-  })
-    .then(response => {
-      if (response.ok) {
-        return response;
-      } else {
-        console.log('Failed to get response');
-      }
-    })
-    .then(response => response.json())
-    .then(data => {
-      if (data.requests !== null) {
-        data.requests.forEach(request => {
-          requests.push(request);
-        });
-      }
-    })
-  return requests;
-}
-
-exports.sendRequest = function() {
-  let emailNode = document.getElementById('request-email');
-  let nameNode = document.getElementById('request-name');
-  let fetchBody = {
-    req_email: emailNode.value,
-    req_name: nameNode.value
-  };
-  let token = sessionStorage.getItem('token');
-  let myHeaders = new Headers();
-  myHeaders.append('x-access-token', token);
-  fetch('/api/v1/requests/send', {
-    method: 'POST',
-    headers: myHeaders
-  })
-    .then(response => {
-      if (response.ok) {
-        return response;
-      } else {
-        console.log('Failed to send request');
-      }
-    })
-    .then(response => response.json())
-    .then(data => {
-      emailNode.value = '';
-      nameNode.value = '';
-    })
-}
-
-exports.sendInvite = function() {
-  let emailNode = document.getElementById('invite-email');
-  let messageNode = document.getElementById('invite-message');
-  messageNode.textContent = null;
-  if (emailNode.value) {
-    let fetchBody = {
-      invite_email: emailNode.value
-    };
-    let token = sessionStorage.getItem('token');
-    let myHeaders = new Headers();
-    myHeaders.append('x-access-token', token);
-    fetch('/api/v1/invite/send', {
-      method: 'POST',
-      headers: myHeaders
-    })
-      .then(response => {
-        if (response.ok) {
-          return response;
-        } else {
-          console.log('Failed to send invite');
-        }
-      })
-      .then(response => response.json())
-      .then(data => {
-        let msg = data.message;
-        messageNode.textContent = msg;
-        emailNode.value = '';
-      })
-  } else {
-    messageNode.textContent = 'You must input an e-mail address.';
-  }
-}
-
-exports.acceptRequest = function(request_info) {
-  let fetchBody = {
-    request: request_info
-  };
-  let token = sessionStorage.getItem('token');
-  let myHeaders = new Headers();
-  myHeaders.append('x-access-token', token);
-  fetch('/api/v1/requests/accept', {
-    method: 'POST',
-    headers: myHeaders
-  })
-    .then(response => {
-      if (response.ok) {
-        return response;
-      } else {
-        console.log('Failed to accept request');
-      }
-    })
-    .then(response => response.json())
-    .then(data => {
-
-    })
-}
