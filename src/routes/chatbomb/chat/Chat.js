18b55d4253d7459b6baad104f65ca7ad23de9f81
Jon Savage Kathe <jonsavagek36@gmail.com>
2017-03-06 07:37:56 -0500

    Update Chat.js

--- src/routes/chatbomb/chat/Chat.js
+++ src/routes/chatbomb/chat/Chat.js
@@ -5,28 +5,33 @@ import ChatBody from './ChatBody';
 import LiveChat from './LiveChat';
 import ChatSend from './ChatSend';
 
-class Chat extends Component {
+export default class Chat extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
   }
-
   render() {
-    let conversation = this.props.conversations.conversations[this.props.selected_friend.id];
     return (
       <div className='main-body'>
         <div className='page-title'>
           <div className='banner'></div>
         </div>
         <div className='chat-body'>
-          <ChatHeader selected_friend={this.props.selected_friend} />
-          <ChatBody conversation={conversation} />
-          <LiveChat live_chat={this.props.live_chat} />
-          <ChatSend sendMessage={this.props.sendMessage} />
+          <ChatHeader
+            selected_friend={this.props.selected_friend}
+              />
+          <ChatBody
+            selected_friend={this.props.selected_friend}
+            conversations={this.props.conversations}
+              />
+          <LiveChat
+            live_chat={this.props.live_chat}
+              />
+          <ChatSend
+            sendLiveChat={this.props.sendLiveChat}
+            sendMessage={this.props.sendMessage}
+              />
         </div>
       </div>
     );
   }
 }
-
-export default Chat;
