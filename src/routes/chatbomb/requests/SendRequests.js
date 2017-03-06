8d96e8bd0c87442426a9540ca10eaf433c1ff938
Jon Savage Kathe <jonsavagek36@gmail.com>
2017-03-06 07:36:12 -0500

    Update SendRequests.js

--- src/routes/chatbomb/requests/SendRequests.js
+++ src/routes/chatbomb/requests/SendRequests.js
@@ -1,36 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class SendRequests extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div>
-        <div className='request-send'>
-          <div className='form-title'>Send Request</div>
-          <div className='form-box'>
-            <div className='form-row'>
-              <div className='form-label'>By E-Mail: </div>
-              <div className='form-input'><input type='text' id='request-email' /></div>
-            </div>
-            <div className='form-message' id='req-email-message'></div>
-            <div className='form-row'>
-              <div className='form-label'>By Screen Name: </div>
-              <div className='form-input'><input type='text' id='request-name' /></div>
-            </div>
-            <div className='form-message' id='req-name-message'></div>
-            <div className='form-buttons'>
-              <div className='signup-btn'><button onClick={this.props.sendRequest}>Send Request</button></div>
-            </div>
-            <div className='form-message' id='req-message'></div>
-          </div>
+const SendRequests = (props) => {
+  return (
+    <div className='request-send'>
+      <div className='form-title'>Send Request</div>
+      <div className='form-box'>
+        <div className='form-row'>
+          <div className='form-label'>By E-Mail: </div>
+          <div className='form-input'><input type='text' id='request-email' /></div>
         </div>
+        <div className='form-message' id='req-email-message'></div>
+        <div className='form-row'>
+          <div className='form-label'>By Screen Name: </div>
+          <div className='form-input'><input type='text' id='request-name' /></div>
+        </div>
+        <div className='form-message' id='req-name-message'></div>
+        <div className='form-buttons'>
+          <div className='signup-btn'><button onClick={props.sendRequest}>Send Request</button></div>
+        </div>
+        <div className='form-message' id='req-message'></div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default SendRequests;
